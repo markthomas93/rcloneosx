da7a0121d97081d14d0680028fd3500eb437f22b
Thomas Evensen <thomeven@gmail.com>
2018-12-27 08:36:32 +0100

    rclone parameters

--- RcloneOSX/Model/RcloneParameters/RcloneParameters.swift
+++ RcloneOSX/Model/RcloneParameters/RcloneParameters.swift
@@ -24,45 +24,29 @@ final class RcloneParameters {
         ("--no-traverse", 0),
         ("--no-gzip-encoding", 0)]
 
-    // Array storing combobox values
+    private let backupString = ["--backup", "--backup-dir=../backup"]
+
     private var comboBoxValues: [String]?
-    // Reference to config
     private var config: Configuration?
 
-    /// Function for getting for rclone arguments to use in ComboBoxes in ViewControllerrcloneParameters
-    /// - parameter none: none
-    /// - return : array of String
     func getComboBoxValues() -> [String] {
         return self.comboBoxValues ?? [""]
     }
 
-    // Computes the raw argument for rclone to save in configuration
-    /// Function for computing the raw argument for rclone to save in configuration
-    /// - parameter indexComboBox: index of selected ComboBox
-    /// - parameter value: the value of rclone parameter
-    /// - return: array of String
-    func getRcloneParameter (indexComboBox: Int, value: String?) -> String {
-        guard  indexComboBox < self.rcloneArguments.count && indexComboBox > -1 else {
-            return ""
-        }
+    func getRcloneParameter(indexComboBox: Int, value: String?) -> String {
+        guard  indexComboBox < self.rcloneArguments.count && indexComboBox > -1 else { return "" }
         switch self.rcloneArguments[indexComboBox].1 {
         case 0:
-            // Predefined rclone argument from combobox
-            // Must check if DELETE is selected
             if self.rcloneArguments[indexComboBox].0 == self.rcloneArguments[1].0 {
                 return ""
             } else {
                 return  self.rcloneArguments[indexComboBox].0
             }
         case 1:
-            // If value == nil value is deleted and return empty string
-            guard value != nil else {
-                return ""
-            }
+            guard value != nil else { return "" }
             if self.rcloneArguments[indexComboBox].0 != self.rcloneArguments[0].0 {
                 return self.rcloneArguments[indexComboBox].0 + "=" + value!
             } else {
-                // Userselected argument and value
                 return value!
             }
         default:
@@ -94,21 +78,14 @@ final class RcloneParameters {
         return [argument!, value!]
     }
 
-    /// Function returns index and value of rclone argument to set the corrospending
-    /// value in combobox when rclone parameters are presented and stored in configuration
     func indexandvaluercloneparameter(_ parameter: String?) -> (Int, String) {
-        guard parameter != nil else {
-            return (0, "")
-        }
+        guard parameter != nil else { return (0, "") }
         let splitstr: [String] = self.split(parameter!)
-        guard splitstr.count > 1 else {
-            return (0, "")
-        }
+        guard splitstr.count > 1 else { return (0, "")}
         let argument = splitstr[0]
         let value = splitstr[1]
         var returnvalue: String?
         var returnindex: Int?
-
         if argument != value && self.indexofrcloneparameter(argument) >= 0 {
             returnvalue = value
             returnindex =  self.indexofrcloneparameter(argument)
@@ -135,15 +112,9 @@ final class RcloneParameters {
         return (returnindex!, returnvalue!)
     }
 
-    /// Function returns value of rclone a touple to set the corrosponding
-    /// value in combobox and the corrosponding rclone value when rclone parameters are presented
-    /// - parameter rcloneparameternumber : which stored rclone parameter, integer 8 - 14
-    /// - returns : touple with index for combobox and corresponding rclone value
     func getParameter (rcloneparameternumber: Int) -> (Int, String) {
         var indexandvalue: (Int, String)?
-        guard self.config != nil else {
-            return (0, "")
-        }
+        guard self.config != nil else { return (0, "")}
         switch rcloneparameternumber {
         case 8:
            indexandvalue = self.indexandvaluercloneparameter(self.config!.parameter8)
@@ -163,6 +134,10 @@ final class RcloneParameters {
             return (0, "")
         }
         return indexandvalue!
+    }
+
+    func getBackupString() -> [String] {
+        return self.backupString
     }
 
     init(config: Configuration) {
